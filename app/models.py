e5c4d92f08b9bfb8f8e77a98eebd7816406e7f78
Alexandr <sasha.abramov2979159@gmail.com>
2024-11-13 00:41:17 +0300

    Add flight fields by theme (goal)

--- app/models.py
+++ app/models.py
@@ -42,7 +42,7 @@ class Flight(models.Model):
     date_complete = models.DateTimeField(verbose_name="Дата завершения", blank=True, null=True)
 
     name = models.CharField(verbose_name="Название", blank=True, null=True)
-    description = models.TextField(verbose_name="Биография", blank=True, null=True)
+    goal = models.TextField(verbose_name="Цель", blank=True, null=True)
     date = models.DateField(verbose_name="Дата", blank=True, null=True)
     is_successful = models.IntegerField(verbose_name="Успех миссии", blank=True, null=True)
 
